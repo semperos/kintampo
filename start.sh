81d35f3d467f1d0439aecc321ebc687df0622fb4
Daniel Gregoire <daniel.l.gregoire@gmail.com>
2018-06-26 13:33:24 -0400

    Correct bash script redirects

--- start.sh
+++ start.sh
@@ -2,9 +2,8 @@
 
 set -e -u
 
-type cargo 2&>1 /dev/null || \
+type cargo 2>&1 >/dev/null || \
     (echo "You need to install Rust and Cargo to proceed. Try rustup." && exit 1)
 
 cd kintampo
-cargo build --release
-RUST_LOG=kintampo=info cargo run
+RUST_LOG=kintampo=trace cargo run
